57d6435e9357b15dfca62103aa9ebea0660642fd
michallves <maicon159951@outlook.com>
2023-09-20 09:16:04 -0300

    first commit

---
+++ src/main/java/edu/pw2/superesportes/controller/LoginController.java
@@ -0,0 +1,10 @@
+package main.java.edu.pw2.superesportes.controller;
+@Controller
+@RequestMapping("/")
+public class LoginController {
+    
+    @GetMapping("login")
+    public String loafLoginForm(){
+
+    }
+}
